6701ed1dbac2454e9fe195ce6ec2b7d3f0b8b0df
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2023-09-08 20:10:15 -0300

    update app.module with new structures

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
+import { LessonModule } from './lesson/lesson.module';
+import { ContentModule } from './content/content.module';
+import { ClassModule } from './class/class.module';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [CatsModule, UsersModule, StudentModule, LessonModule, ContentModule, ClassModule],
   controllers: [AppController],
   providers: [AppService],
 })
