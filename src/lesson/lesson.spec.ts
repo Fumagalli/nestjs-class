3a797c4c10a9db7a52a86c7b20fa61dd170d5294
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2023-09-15 17:38:12 -0300

    remove mock modules from old project

--- src/lesson/lesson.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Lesson } from './lesson';
-
-describe('Lesson', () => {
-  it('should be defined', () => {
-    expect(new Lesson()).toBeDefined();
-  });
-});
--- src/student/student.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Student } from './student';
-
-describe('Student', () => {
-  it('should be defined', () => {
-    expect(new Student()).toBeDefined();
-  });
-});
