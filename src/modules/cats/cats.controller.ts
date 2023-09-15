48c2d9b88f4cf9bc86465d8fa4b59c98b0bb25ba
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2023-09-15 17:39:13 -0300

    Create 'modules' folder and move current modules to it

---
+++ src/modules/cats/cats.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get(':name')
+  findOne(@Param('name') name: string) {
+    return this.catsService.findOne(name);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+}
