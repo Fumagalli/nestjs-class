e9713f6923a33069e9667541cbda982ce38c88b7
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2023-09-15 17:39:58 -0300

    update project to use graphql + typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { CatsModule } from './cats/cats.module';
-import { UsersModule } from './users/users.module';
+import { CatsModule } from './modules/cats/cats.module';
+import { UsersModule } from './modules/users/users.module';
 import { AppService } from './app.service';
-import { StudentModule } from './student/student.module';
-import { LessonModule } from './lesson/lesson.module';
-import { ContentModule } from './content/content.module';
-import { ClassModule } from './class/class.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { StudentsModule } from './modules/students/students.module';
 
 @Module({
-  imports: [CatsModule, UsersModule, StudentModule, LessonModule, ContentModule, ClassModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    StudentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
