f49fcd58b5aa691a01ecd185b549930481b1b55e
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2023-09-29 15:10:23 -0300

    chore: resolving merge issue

--- src/modules/lessons/dto/create-lesson.input.ts
+++ src/modules/lessons/dto/create-lesson.input.ts
@@ -2,9 +2,5 @@ import { InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateLessonInput {
-<<<<<<< HEAD
-=======
-  @Field(() => String)
->>>>>>> 0a2da38b1a71d3256900430fecb433d585816898
   description: string;
 }
