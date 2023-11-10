fd13f5febf4ca2af1bea07bba0215fbb31f52c52
pcsenac <pcsenac@senac.com>
2023-11-10 20:48:19 -0300

    add JWT to project

--- nestjs-typeorm/src/app.module.ts
+++ nestjs-typeorm/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './modules/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database/database.module';
 import { ItemsModule } from './modules/items/items.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     ItemsModule,
+    AuthModule,
+    UsersModule
   ],
   controllers: [],
   providers: [],
