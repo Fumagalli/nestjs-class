cb2ee723bb6b693eaa8b4410116ae586f2273125
Ângelo Fumagalli <fumagalli.angelo@gmail.com>
2023-11-24 11:35:19 -0300

    feat: add auth to core post requests

--- nestjs-typeorm/src/modules/items/items.controller.ts
+++ nestjs-typeorm/src/modules/items/items.controller.ts
@@ -6,15 +6,18 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   async create(@Body() createItemDto: CreateItemDto) {
     return this.itemsService.create(createItemDto);
@@ -30,11 +33,13 @@ export class ItemsController {
     return this.itemsService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateItemDto: UpdateItemDto) {
     return this.itemsService.update(+id, updateItemDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return this.itemsService.remove(+id);
